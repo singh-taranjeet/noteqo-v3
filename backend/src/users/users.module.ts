f03f6526fefe8bdb64fb8951d4adf5b4a29c801d
Taranjeet Singh <93113908+singh-taranjeet@users.noreply.github.com>
2026-03-28 08:51:14 +1000

    feat(backend): add user registration DTO validation and response mapping

---
+++ backend/src/users/users.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { UsersRepository } from './users.repository';
+import { UserEntity } from './entities/user.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([UserEntity])],
+  controllers: [UsersController],
+  providers: [UsersService, UsersRepository],
+  exports: [UsersService],
+})
+export class UsersModule {}
