ebfea8ea98367d0ed6396e050b1675cac4f45b9c
singh-taranjeet <93113908+singh-taranjeet@users.noreply.github.com>
2026-04-11 16:20:57 +1000

    card component

---
+++ frontend/features/editor/components/nodes/CardNode/CardNodeExtension.ts
@@ -0,0 +1,33 @@
+import { Node as TiptapNode, mergeAttributes } from "@tiptap/core";
+import { ReactNodeViewRenderer } from "@tiptap/react";
+import { CardNodeView } from "./CardNodeView";
+
+export const CardNodeExtension = TiptapNode.create({
+  name: "shadcnCard",
+  group: "block",
+  content: "block+",
+
+  addAttributes() {
+    return {
+      colorTheme: {
+        default: "default",
+      },
+    };
+  },
+
+  parseHTML() {
+    return [{ tag: "div[data-type='shadcn-card']" }];
+  },
+
+  renderHTML({ HTMLAttributes }: Record<string, any>) {
+    return [
+      "div",
+      mergeAttributes(HTMLAttributes, { "data-type": "shadcn-card" }),
+      0,
+    ];
+  },
+
+  addNodeView() {
+    return ReactNodeViewRenderer(CardNodeView);
+  },
+});
