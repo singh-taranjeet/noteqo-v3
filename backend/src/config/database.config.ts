9911d4cf1628a8ff2454da39863cc98865c8723f
singh-taranjeet <93113908+singh-taranjeet@users.noreply.github.com>
2026-04-30 07:52:50 +1000

    deploy

--- backend/src/config/database.config.ts
+++ backend/src/config/database.config.ts
@@ -2,6 +2,7 @@ import { registerAs } from '@nestjs/config';
 import { CONFIG_KEYS } from './config.constants';
 
 export const databaseConfig = registerAs(CONFIG_KEYS.DATABASE, () => ({
+  url: process.env.DATABASE_URL,
   host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT || '5432', 10),
   name: process.env.DB_NAME,
