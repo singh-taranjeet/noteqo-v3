78a843e548b50cbb129dc9ff9070d993ebb1bb57
Taranjeet Singh <93113908+singh-taranjeet@users.noreply.github.com>
2026-05-16 14:18:14 +1000

    fixed bg-glass effect

--- frontend/src/layouts/WorkspaceLayout.tsx
+++ frontend/src/layouts/WorkspaceLayout.tsx
@@ -11,11 +11,15 @@ export function WorkspaceLayout() {
         <AppShell>
           <SidebarProvider>
             <AppSidebar />
-            <SidebarInset>
-              <Header />
-              <main className="flex-1 overflow-auto">
-                <Outlet />
-              </main>
+            <SidebarInset className="shadow-none! rounded-none! pt-0! bg-transparent!">
+              <div className="shadow-xl absolute left-1 top-1 flex h-[calc(100vh-8px)] w-[calc(100%-8px)] flex-col overflow-hidden rounded-xl border bg-background">
+                <Header />
+                <div className="flex-1 overflow-auto">
+                  <div className="pt-11">
+                    <Outlet />
+                  </div>
+                </div>
+              </div>
             </SidebarInset>
           </SidebarProvider>
         </AppShell>
